1120c50df34963949215c4059cd66fd75c63ea58
Mahesh Pattar <mahesh.pattar@ltts.com>
2020-08-08 22:44:22 +0530

    xml comments

--- MovieManagerMicroService/DTO/CityDTO.cs
+++ MovieManagerMicroService/DTO/CityDTO.cs
@@ -6,10 +6,20 @@ using System.Threading.Tasks;
 
 namespace MovieManagerMicroService.DTO
 {
+    /// <summary>
+    /// City DTO
+    /// </summary>
     public class CityDTO
     {
+        /// <summary>
+        /// Gets or sets City id
+        /// </summary>
         [Key]
         public int CityId { get; set; }
+
+        /// <summary>
+        /// Gets or sets City name
+        /// </summary>
         public string CityName { get; set; }
     }
 }
